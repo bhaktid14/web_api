a96f5fd78a8afb3769075b6fffbcd595bdcfa00d
unknown <bhaktidukare14@gmail.com>
2023-03-10 01:27:22 +0530

    commit

--- crud_application_web_api/Controllers/emp2Controller.cs
+++ crud_application_web_api/Controllers/emp2Controller.cs
@@ -1,9 +1,11 @@
 ﻿using crud_application_web_api.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Net.Http;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 
 namespace crud_application_web_api.Controllers
@@ -16,7 +18,7 @@ namespace crud_application_web_api.Controllers
         {
             List<Employee> emp_list =new List<Employee>();
             client.BaseAddress = new Uri("");
-            var response = client.GetAsync("");
+            var response = client.GetAsync("emp");
             response.Wait();
 
             var test = response.Result;
@@ -26,5 +28,78 @@ namespace crud_application_web_api.Controllers
             }
             return View(emp_list);
         }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [System.Web.Http.HttpPost]
+        public ActionResult Create(Employee emp)
+        {
+            client.BaseAddress = new Uri("");
+            var response = client.PostAsJsonAsync<Employee>("emp",emp);
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("index");
+            }
+            return View("Create");
+        }
+        public ActionResult Details(int id)
+        {
+            Employee e = null;
+            client.BaseAddress = new Uri("");
+            var response = client.GetAsync("emp?T_id=" + id.ToString());
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+          
+        
+                var display = test.Content.ReadAsAsync<Employee>();
+                display.Wait();
+
+                e = display.Result;
+            }
+            return View(e);
+        }
+        public ActionResult Edit(int id)
+        {
+            Employee e = null;
+            client.BaseAddress = new Uri("");
+            var response = client.GetAsync("emp?T_id=" + id.ToString());
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+
+
+                var display = test.Content.ReadAsAsync<Employee>();
+                display.Wait();
+
+                e = display.Result;
+            }
+            return View(e);
+        }
+        [System.Web.Http.HttpPost]
+        public ActionResult Edit(Employee e) {
+            client.BaseAddress = new Uri("");
+            var response = client.PutAsJsonAsync<Employee>("emp", e);
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("index");
+            }
+            return View("Edit");
+        }
+
     }
+    
+    
 }
--- crud_application_web_api/Controllers/empController.cs
+++ crud_application_web_api/Controllers/empController.cs
@@ -24,8 +24,50 @@ namespace crud_application_web_api.Controllers
                 List<Employee> list = db.Employees.ToList();
                 return Ok();
             }
-               
+        }
 
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult GetEmployeesById(int id)
+        {
+            using (web_api_crud_dbEntities1 entities = new web_api_crud_dbEntities1())
+            {
+                var emp = db.Employees.Where(model => model.T_id == id).FirstOrDefault();
+                return Ok(emp);
+            }
+        }
+
+
+        [System.Web.Http.HttpPost]
+        public IHttpActionResult EmpInsert(Employee e)
+        {
+            using (web_api_crud_dbEntities1 entities = new web_api_crud_dbEntities1())
+            {
+                db.Employees.Add(e);
+                db.SaveChanges();
+                return Ok();
+            }
+        }
+
+        [System.Web.Http.HttpPut]
+        public IHttpActionResult EmpUpdate(Employee e)
+        {
+            using (web_api_crud_dbEntities1 entities = new web_api_crud_dbEntities1())
+            {
+                var emp = db.Employees.Where(model => model.T_id == e.T_id).FirstOrDefault();
+                if(emp != null)
+                {
+                    emp.T_id = e.T_id;
+                    emp.T_name= e.T_name;   
+                    emp.T_priority = e.T_priority;  
+                    emp.Date= e.Date;
+                    db.SaveChanges();
+                }
+                else
+                {
+                    return NotFound();
+                }
+                return Ok();
+            }
         }
     }
 }
