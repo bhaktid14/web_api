80d852fac57424aca486d3efa6f7bc849097921a
unknown <bhaktidukare14@gmail.com>
2023-03-09 19:12:06 +0530

    commit

---
+++ crud_application_web_api/Controllers/emp2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace crud_application_web_api.Controllers
+{
+    public class emp2Controller : Controller
+    {
+        // GET: emp2
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ crud_application_web_api/Controllers/empController.cs
@@ -0,0 +1,31 @@
+﻿using crud_application_web_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Net.Http;
+using System.Web.Mvc;
+using System.Data.Entity;
+using System.Web.Http;
+using System.Web.UI.WebControls;
+
+namespace crud_application_web_api.Controllers
+{
+    public class empController : ApiController
+    {
+        web_api_crud_dbEntities1 db = new web_api_crud_dbEntities1();
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult GetEmployees()
+        {
+            using (web_api_crud_dbEntities1 entities = new web_api_crud_dbEntities1())
+            {
+                List<Employee> list = db.Employees.ToList();
+                return Ok();
+            }
+               
+
+        }
+    }
+}
