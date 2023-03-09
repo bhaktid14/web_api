5b40111d002c6df309c6c8d9ebd430dec326f056
unknown <bhaktidukare14@gmail.com>
2023-03-09 20:04:30 +0530

    second commit

--- crud_application_web_api/Controllers/emp2Controller.cs
+++ crud_application_web_api/Controllers/emp2Controller.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using crud_application_web_api.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,9 +11,20 @@ namespace crud_application_web_api.Controllers
     public class emp2Controller : Controller
     {
         // GET: emp2
+        HttpClient client = new HttpClient();
         public ActionResult Index()
         {
-            return View();
+            List<Employee> emp_list =new List<Employee>();
+            client.BaseAddress = new Uri("");
+            var response = client.GetAsync("");
+            response.Wait();
+
+            var test = response.Result;
+            if(test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<List<Employee>>();
+            }
+            return View(emp_list);
         }
     }
 }
